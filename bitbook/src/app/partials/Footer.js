89181c116bd00b38da0c14b2c01507fb71e140ce
student <student>
2018-06-12 15:52:53 +0200

    Header and Footer

---
+++ bitbook/src/app/partials/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Footer = props => {
+    return <div className="">
+        <h4 className="footer">@ BIT-blog {props.name}</h4>
+    </div>;
+};
+
+export default Footer;
