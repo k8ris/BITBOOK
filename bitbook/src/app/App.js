712e5cc3966a5e1916e19d3dd673958681f42b1a
student <studentm>
2018-06-13 17:07:39 +0200

    merge

--- bitbook/src/app/App.js
+++ bitbook/src/app/App.js
@@ -3,13 +3,10 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Header from './partials/Header';
 import Footer from './partials/Footer';
-<<<<<<< HEAD
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import FeedList from './feedPage/FeedLIst';
-=======
 import MainFeed from './feedPage/MainFeed';
->>>>>>> change make MainFeed and add FeedList into that
 import ProfilePage from './profilePage/ProfilePage';
 import PeoplePage from './peoplePage/PeoplePage';
 
@@ -20,16 +17,9 @@ class App extends Component {
       <div>
         <Header />
         <Switch>
-
-<<<<<<< HEAD
-<<<<<<< HEAD
           <Route exact path='/' component={FeedList} />
-=======
->>>>>>> add button
           <Route path='/Feed' component={FeedList} />
-=======
           <Route path='/Feed' component={MainFeed} />
->>>>>>> change make MainFeed and add FeedList into that
           <Route path='/People' component={PeoplePage} />
           <Route path='/Profile' component={ProfilePage} />
         </Switch>
--- bitbook/src/app/feedPage/FeedLIst.js
+++ bitbook/src/app/feedPage/FeedLIst.js
@@ -1,66 +1,6 @@
 import React from 'react';
 import FetchData from "../../service/ServiceGetData"
-<<<<<<< HEAD
 import FeedButton from './FeedButton'
-
-class FeedList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: [],
-        };
-    }
-
-    componentDidMount() {
-        FetchData.getPosts().then(posts => {
-            this.setState({ posts: posts });
-        });
-    }
-
-    render() {
-
-        return (<div className='feedPosts'>
-            {this.state.posts.map(post => {
-                if (post.type === 'video') {
-                    return (<div className="postBody">
-                        <iframe src={post.videoUrl} />
-                        <div>
-                            <p className="card-text">Video post</p>
-                            <a href="#" className="card-link">Another link</a>
-                        </div>
-                    </div>)
-                }
-
-                else if (post.type === 'text') {
-                    return (<div className="postBody">
-                        {post.text}
-                        <br />
-                        <br />
-                        <br />
-                        <p className="card-text">Text post</p>
-                        <a href="#" className="card-link">Another link</a>
-                    </div>)
-                }
-                else if (post.type === 'image') {
-                    return (<div className="postBody">
-                        <img src={post.imageUrl} />
-                        <p className="card-text">Image Post</p>
-                        <a href="#" className="card-link">Another link</a>
-                    </div>)
-                }
-            })
-
-            }
-<<<<<<< HEAD
-        </div>)
-
-=======
-            <FeedButton  />
-        </ div>)
->>>>>>> add button
-    }
-}
-=======
 
 const FeedList = (props) => {
 
@@ -83,11 +23,4 @@ const FeedList = (props) => {
 }
 
 export default FeedList;
-
-
->>>>>>> change make MainFeed and add FeedList into that
-
-export default FeedList;
-
-
 
