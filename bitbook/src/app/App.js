6c542d5c29c64979a2cf6e29fda3171bd86729eb
student <studentm>
2018-06-13 16:32:45 +0200

    change make MainFeed and add FeedList into that

--- bitbook/src/app/App.js
+++ bitbook/src/app/App.js
@@ -3,9 +3,13 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Header from './partials/Header';
 import Footer from './partials/Footer';
+<<<<<<< HEAD
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import FeedList from './feedPage/FeedLIst';
+=======
+import MainFeed from './feedPage/MainFeed';
+>>>>>>> change make MainFeed and add FeedList into that
 import ProfilePage from './profilePage/ProfilePage';
 import PeoplePage from './peoplePage/PeoplePage';
 
@@ -18,10 +22,14 @@ class App extends Component {
         <Switch>
 
 <<<<<<< HEAD
+<<<<<<< HEAD
           <Route exact path='/' component={FeedList} />
 =======
 >>>>>>> add button
           <Route path='/Feed' component={FeedList} />
+=======
+          <Route path='/Feed' component={MainFeed} />
+>>>>>>> change make MainFeed and add FeedList into that
           <Route path='/People' component={PeoplePage} />
           <Route path='/Profile' component={ProfilePage} />
         </Switch>
--- bitbook/src/app/feedPage/FeedLIst.js
+++ bitbook/src/app/feedPage/FeedLIst.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FetchData from "../../service/ServiceGetData"
+<<<<<<< HEAD
 import FeedButton from './FeedButton'
 
 class FeedList extends React.Component {
@@ -59,6 +60,32 @@ class FeedList extends React.Component {
 >>>>>>> add button
     }
 }
+=======
+
+const FeedList = (props) => {
+
+    return (<div className='feedPosts'>
+        {props.posts.map(post => {
+            if (post.type === 'video') {
+                return (<iframe width="854" height="400" src={post.videoUrl} allow="autoplay; encrypted-media" allowfullscreen></iframe>)
+            }
+
+            else if (post.type === 'text') {
+                return (<div>{post.text}</div>)
+            }
+            else if (post.type === 'image') {
+                return (<div> <img src={post.imageUrl} width="854" height="400" /></div>)
+            }
+        })
+        }
+    
+    </ div>)
+}
+
+export default FeedList;
+
+
+>>>>>>> change make MainFeed and add FeedList into that
 
 export default FeedList;
 
