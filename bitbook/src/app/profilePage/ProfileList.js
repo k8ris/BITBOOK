690a54b18af8310b2c0edcb6b2a7f2016905deeb
student <studentm>
2018-06-14 17:55:01 +0200

    profile page is finished

--- bitbook/src/app/profilePage/ProfileList.js
+++ bitbook/src/app/profilePage/ProfileList.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 const ProfileList = (props) => {
     return (<div>
-        <img src={props.profile.avatarUrl} />
-        <p>{props.profile.name}</p>
+        <img className="profilePic" src={props.profile.avatarUrl} />
+        <h4 className="profileName">{props.profile.name}</h4>
         <p>{props.profile.about}</p>
-        <p>{props.profile.commentsCount}</p>
-        <p>{props.profile.postsCount}</p>
-
+        <button type="button" class="btn">
+        <span class="badge badge-pill">{props.profile.postsCount} Posts</span>
+</button>
+<button type="button" class="btn">
+<span class="badge badge-pill">{props.profile.commentsCount} Comments</span>
+</button>
 
     </div>)
 }
@@ -15,3 +18,6 @@ const ProfileList = (props) => {
 
 
 export default ProfileList;
+
+
+
