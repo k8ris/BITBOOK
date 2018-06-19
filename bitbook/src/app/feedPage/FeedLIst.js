d8d89eb043abbfeed90ffad7227650699c6d8857
student <student>
2018-06-19 17:49:11 +0200

    feedlist

--- bitbook/src/app/feedPage/FeedLIst.js
+++ bitbook/src/app/feedPage/FeedLIst.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Text, Image, Video } from './FeedItem';
 
 const FeedLIst = (props) => {
-    return (
-        <div className='feedPosts'>
-            {props.posts.map(post => {
-                // 1.pitanje
-                if (post.type === 'video') { return <Video post={post} /> }
-                else if (post.type === 'text') { return <Text post={post} /> }
-                else if (post.type === 'image') { return <Image post={post} /> }
-            }
-            )
-            }
-        </div>)
+        return (
+                <div className='feedPosts'>
+                        {props.posts.map(post => {
+                                // 1.pitanje
+                                if (post.type === 'video') { return <Video post={post} /> }
+                                else if (post.type === 'text') { return <Text post={post} /> }
+                                else if (post.type === 'image') { return <Image post={post} /> }
+                        }
+                        )
+                        }
+                </div>)
 }
 export default FeedLIst;
 
